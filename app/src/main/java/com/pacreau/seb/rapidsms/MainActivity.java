82be3166da0b11440bab582931c0ac259a952ce3
Pacreau Sebastien <sebastien.pacreau@soprasteria.com>
2016-11-15 13:55:09 +0100

    version 1 fonctionnel

---
+++ app/src/main/java/com/pacreau/seb/rapidsms/MainActivity.java
@@ -0,0 +1,217 @@
+package com.pacreau.seb.rapidsms;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
+import android.support.v7.widget.Toolbar;
+import android.telephony.SmsManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
+    private static final int DURATION_WAIT_IN_MS = 5000;
+    public static final String SENDING_SMS = "SENDING_SMS";
+    public static final String LAST_SENDING_DATE = "LAST_SENDING_DATE";
+    public static final String LAST_SENDING_TO = "LAST_SENDING_TO";
+    public static final String LAST_SENDING_CONTENT = "LAST_SENDING_CONTENT";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                view.setEnabled(false);
+                sendSms(view);
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (view != null) {
+                            view.setEnabled(true);
+                        }
+                    }
+                }, DURATION_WAIT_IN_MS);
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        if (this.getIntent().getBooleanExtra(SENDING_SMS,true)) {
+            sendSms(drawer);
+        } else {
+            String sPhoneNumber = PreferenceManager.getDefaultSharedPreferences(this).getString(LAST_SENDING_TO, "");
+            String sDate = PreferenceManager.getDefaultSharedPreferences(this).getString(LAST_SENDING_DATE, "");
+            String sContent = PreferenceManager.getDefaultSharedPreferences(this).getString(LAST_SENDING_CONTENT, "");
+            changeUIDesign(drawer, sPhoneNumber,sDate,sContent);
+        }
+    }
+
+    private void sendSms(final View p_view) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.SEND_SMS},
+                        MY_PERMISSIONS_REQUEST_SEND_SMS);
+            }
+        }
+        String sPhoneNumber = PreferenceManager
+                .getDefaultSharedPreferences(this)
+                .getString(SettingsActivity.PHONE_NUMBER_PREF_KEY, "");
+        try {
+            String sMessage = PreferenceManager
+                    .getDefaultSharedPreferences(this)
+                    .getString(SettingsActivity.MESSAGE_KEY_PREF, "");
+
+            //SmsManager.getDefault().sendTextMessage(sPhoneNumber, null, sMessage, null, null);
+
+            SimpleDateFormat oFormatDate = new SimpleDateFormat("dd/MM/yyy HH:mm:ss");
+            String sDate = oFormatDate.format(new Date());
+
+            PreferenceManager
+                    .getDefaultSharedPreferences(this).edit()
+                    .putString(LAST_SENDING_DATE,sDate)
+                    .putString(LAST_SENDING_TO, sPhoneNumber)
+                    .putString(LAST_SENDING_CONTENT, sMessage).commit();
+
+            changeUIDesign(p_view, sPhoneNumber, sDate, sMessage);
+            sendLocalNotification(sPhoneNumber, sMessage , sDate);
+
+        } catch (Exception e) {
+            Snackbar.make(p_view, "Message pas envoyé au " + sPhoneNumber, Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }
+    }
+
+    private void changeUIDesign(View p_view, String p_sPhoneNumber , String p_sDate, String p_sContent) {
+        Snackbar.make(p_view, "Message envoyé au " + p_sPhoneNumber, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+
+        TextView oTextView = (TextView) findViewById(R.id.lastMessageDate);
+        oTextView.setText(p_sDate);
+
+        oTextView = (TextView) findViewById(R.id.lastMessageTo);
+        oTextView.setText(p_sPhoneNumber);
+
+        oTextView = (TextView) findViewById(R.id.lastMessageContent);
+        oTextView.setText(p_sContent);
+    }
+
+    private void sendLocalNotification(String p_sPhoneNumber, String p_sMessage, String p_sDate) {
+        Context ctx = this.getApplicationContext();
+
+        String title = "RapidSMS envoyé au " + p_sPhoneNumber ;
+        String content = "le " + p_sDate ;
+        NotificationCompat.Builder notifBuilder = new NotificationCompat.Builder(ctx);
+        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        notifBuilder
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setTicker(title)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setDefaults(Notification.DEFAULT_LIGHTS| Notification.DEFAULT_SOUND);
+        notifBuilder.setSound(uri);
+
+        // Add Big View Specific Configuration
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+        inboxStyle.setBigContentTitle(title);
+        inboxStyle.addLine(content).addLine( p_sMessage);
+        notifBuilder.setStyle(inboxStyle);
+
+        NotificationManager notificationManager = (NotificationManager) ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notifBuilder.build());
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent oIntent = new Intent(this, SettingsActivity.class);
+            this.startActivity(oIntent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_manage) {
+            // Handle the camera action
+            Intent oIntent = new Intent(this, SettingsActivity.class);
+            this.startActivity(oIntent);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
